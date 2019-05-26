dcc45e499d37e666696c522c95fa2e693672c357
Edward Cheng <eds_dev@outlook.com>
2019-05-26 21:14:33 +0800

    add editorconfig use docking branch of imgui add a handful of utilities

--- include/imgui_utils.h
+++ include/imgui_utils.h
@@ -2,9 +2,77 @@
 
 namespace ImGui
 {
-    void SetNextWindowMaximized()
-    {
-        ImGui::SetNextWindowPos({0, 0});
-        ImGui::SetNextWindowSize(ImGui::GetIO().DisplaySize);
-    }
+	inline void SetNextWindowMaximized()
+	{
+		ImGui::SetNextWindowPos({ 0, 0 });
+		ImGui::SetNextWindowSize(ImGui::GetIO().DisplaySize);
+	}
+
+	//
+	//
+	inline bool InputInt2(const char* label, int* x0, int* x1, ImGuiInputTextFlags flags = 0)
+	{
+		int xs[]{ *x0, *x1 };
+		bool result = InputInt2(label, xs, flags);
+
+		*x0 = xs[0];
+		*x1 = xs[1];
+		return result;
+	}
+	inline bool InputInt3(const char* label, int* x0, int* x1, int* x2,
+		ImGuiInputTextFlags flags = 0)
+	{
+		int xs[]{ *x0, *x1, *x2 };
+		bool result = InputInt3(label, xs, flags);
+
+		*x0 = xs[0];
+		*x1 = xs[1];
+		*x2 = xs[3];
+		return result;
+	}
+	inline bool InputInt4(const char* label, int* x0, int* x1, int* x2, int* x3,
+		ImGuiInputTextFlags flags = 0)
+	{
+		int xs[]{ *x0, *x1, *x2, *x3 };
+		bool result = InputInt4(label, xs, flags);
+
+		*x0 = xs[0];
+		*x1 = xs[1];
+		*x2 = xs[2];
+		*x3 = xs[3];
+		return result;
+	}
+
+	inline bool InputFloat2(const char* label, float* x0, float* x1, ImGuiInputTextFlags flags = 0)
+	{
+		float xs[]{ *x0, *x1 };
+		bool result = InputFloat2(label, xs, flags);
+
+		*x0 = xs[0];
+		*x1 = xs[1];
+		return result;
+	}
+	inline bool InputFloat3(const char* label, float* x0, float* x1, float* x2,
+		ImGuiInputTextFlags flags = 0)
+	{
+		float xs[]{ *x0, *x1, *x2 };
+		bool result = InputFloat3(label, xs, flags);
+
+		*x0 = xs[0];
+		*x1 = xs[1];
+		*x2 = xs[3];
+		return result;
+	}
+	inline bool InputFloat4(const char* label, float* x0, float* x1, float* x2, float* x3,
+		ImGuiInputTextFlags flags = 0)
+	{
+		float xs[]{ *x0, *x1, *x2, *x3 };
+		bool result = InputFloat4(label, xs, flags);
+
+		*x0 = xs[0];
+		*x1 = xs[1];
+		*x2 = xs[2];
+		*x3 = xs[3];
+		return result;
+	}
 } // namespace ImGui
