e54c936d212bdb785aebc229bbef05c04dfad957
Edward Cheng <eds_dev@outlook.com>
2019-09-13 11:53:19 +0800

    refine code as a library

--- include/backend.h
+++ include/backend.h
@@ -5,25 +5,33 @@
 class DynamicTexture
 {
 public:
-	using Ptr = std::unique_ptr<DynamicTexture>;
+    using Ptr = std::unique_ptr<DynamicTexture>;
 
-    DynamicTexture()         = default;
+    DynamicTexture()          = default;
     virtual ~DynamicTexture() = default;
 
-	// size of buffer is width_ * height_ * 4, i.e. RGBA
+    // size of buffer is width_ * height_ * 4, i.e. RGBA
     virtual void Update(std::function<void(void* p, int row_pitch)> f) = 0;
 
-	auto Id() const { return id_; }
-	auto Width() const { return width_; }
-	auto Height() const { return height_; }
+    auto Id() const
+    {
+        return id_;
+    }
+    auto Width() const
+    {
+        return width_;
+    }
+    auto Height() const
+    {
+        return height_;
+    }
 
 protected:
-	ImTextureID id_;
-	int width_;
-	int height_;
+    ImTextureID id_ = nullptr;
+    int width_      = 0;
+    int height_     = 0;
 };
 
 int RunApplication(Application& app);
 
 DynamicTexture::Ptr AllocateTexture(int width, int height);
-
